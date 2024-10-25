f0bced9f97c5483bfffca1c5263768ff22ec1b46
ISKYEN <150463747+IsaacYen@users.noreply.github.com>
2024-10-26 00:21:05 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/IsaacYen/live2d_api/",
+			cdnPath: "https://fastly.jsdelivr.net/gh/IsaacYen/live2d_api/model/ShizukuTalk",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
